bf35c0d98a05fa314262b035b747b9f7d8c3ecb2
lone <tsuyoku2024@gmail.com>
2026-04-14 00:46:14 +0800

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,10 +1,14 @@
 def bs(lo, hi):
   while lo < hi:
-    res = -1
-    mid = lo + (hi-lo)//2
-    if mid:
-      hi = mid - 1
-      res = mid
-    else:
-      lo = mid + 1
-  return mid
+    mid = (lo + hi) // 2
+    if ok(mid): hi = mid
+    else: lo = mid + 1
+  return lo
+
+def bs(lo, hi):
+  while lo < hi:
+    mid = (lo + hi + 1) // 2
+    if ok(mid): lo = mid
+    else: hi = mid - 1
+  return hi
+# (lo = mid) : +1
