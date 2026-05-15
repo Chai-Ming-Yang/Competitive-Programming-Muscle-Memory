cfc6f685c96b097fbf4f2aec3f8dcf79732acb67
lone <tsuyoku2024@gmail.com>
2026-05-15 09:11:56 +0800

    Refactor LL-Utils and add cycleStart function
    Refactor linked list utility functions for clarity and correctness. Added cycleStart function to find the start of a cycle in the linked list.

--- LL-Utils.py
+++ LL-Utils.py
@@ -1,25 +1,25 @@
 def reverse(head):
   pre, cur = None, head
   while cur:
-    tmp = cur.next
+    nxt = cur.next
     cur.next = pre
     pre = cur
-    cur = tmp
+    cur = nxt
   return pre
 
 def findMid(head):
   slow = fast = head
-  while fast:
+  while fast and fast.next:
     slow = slow.next
     fast = fast.next.next
   return slow
 
 def hasCycle(head):
-  slow, fast = head, head.next
-  while fast:
-    if fast != slow: return True
+  slow = fast = head
+  while fast and fast.next:
     slow = slow.next
     fast = fast.next.next
+    if slow == fast: return True
   return False
 
 def merge(l1, l2):
@@ -34,3 +34,17 @@ def merge(l1, l2):
     cur = cur.next
   cur.next = l1 or l2
   return dummy.next
+
+def cycleStart(head):
+  slow = fast = head
+  while fast and fast.next:
+    slow = slow.next
+    fast = fast.next.next
+    if slow == fast: break
+  else:
+    return None
+  trail = head
+  while slow != trail:
+    trail = trail.next
+    slow = slow.next
+  return slow
